7b0b82e3b1339d24f1ad6010ba9959aca8b0ea5f
dmastrop <dave.mastropolo@gmail.com>
2023-06-04 19:55:53 -0700

    Revert "break the code with .not."
    This reverts commit 8aa4ee75f9c5fdd2433c034011eab9b4f148de83.

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -17,6 +17,6 @@ describe('MainContent', () => {
     const button = screen.getByRole('button');
     await userEvent.click(button);
     //can break the code here by adding in .not.
-    expect(screen.getByTestId('help-area')).not.toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
